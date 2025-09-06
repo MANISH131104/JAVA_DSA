84701ddf3bd5a68b1f928629e847a87de83dfb33
manish131104 <manishkumar117694@gmail.com>
2025-09-06 23:03:04 +0530

    code

---
+++ src/Strings/palindrome.java
@@ -0,0 +1,25 @@
+package Strings;
+
+public class palindrome {
+    public static void main(String[] args) {
+        String s="malayalam";
+        int n=s.length();
+        System.out.println(s);
+        boolean flag=false;
+        int i=0;
+        int j=n-1;
+        while(i<=j){
+            if(s.charAt(i)==s.charAt(j)) {
+                flag=true;
+                i++;
+                j--;
+            }
+        }
+        if(flag==true){
+            System.out.println("malayalam is palindrome");
+        }
+        else{
+            System.out.println("malayalam is not palindrome");
+        }
+    }
+}
