1d0ce5caecd7fba51fc82294807391a976fbdf22
manish131104 <manishkumar117694@gmail.com>
2025-09-08 19:37:14 +0530

    code

--- src/Strings/countDigits.java
+++ src/Strings/countDigits.java
@@ -4,8 +4,13 @@ import java.util.Scanner;
 public class countDigits {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
+//        int n=sc.nextInt();
+//        String s=""+n;
+
+        double n=sc.nextDouble();
         String s=""+n;
         System.out.println(s.length());
+
+
     }
 }
---
+++ src/Strings/stringToInt.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class stringToInt {
+    public static void main(String[] args) {
+        String s="1327";
+        int n=Integer.parseInt(s);
+        System.out.println(n+1);
+    }
+}
