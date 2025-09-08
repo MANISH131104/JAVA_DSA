bb41eb5c5a16d57792ff0081926bb740c27183c1
manish131104 <manishkumar117694@gmail.com>
2025-09-08 18:35:08 +0530

    code

--- src/Strings/plus.java
+++ src/Strings/plus.java
@@ -3,6 +3,10 @@ package Strings;
 public class plus {
     public static void main(String[] args) {
         String s="manish";
+//        s=10+s;
+//        System.out.println(s);            // 10manish
+
+
         s=s+" tannu ";
         s+='\n';                    // line change hota hai
         s+=2;
---
+++ src/Strings/predictTheOutput.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class predictTheOutput {
+    public static void main(String[] args) {
+        String s="manish";
+        System.out.println(s);
+        System.out.println("manish"+(10+20));
+        System.out.println(10+"manish"+20);
+        System.out.println(10+20+"manish");
+    }
+}
