9b8765f27ce3c7021ab3f63617713515ee1597d2
manish131104 <manishkumar117694@gmail.com>
2025-09-08 19:16:26 +0530

    code

--- src/Strings/countDigits.java
+++ src/Strings/countDigits.java
@@ -1,13 +1,11 @@
 package Strings;
 import java.util.Scanner;
 
-public class IntToString {
+public class countDigits {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-//        String s="";            // empty string
-//        s+=n;
-        String s=Integer.toString(n);
-        System.out.println(s);
+        String s=""+n;
+        System.out.println(s.length());
     }
 }
