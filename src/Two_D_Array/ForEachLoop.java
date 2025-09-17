eb8a592ca8c4c66c189f46e7b4153ed5369f7b94
manish131104 <manishkumar117694@gmail.com>
2025-09-17 22:21:51 +0530

    code

--- src/Two_D_Array/ForEachLoop.java
+++ src/Two_D_Array/ForEachLoop.java
@@ -1,11 +1,11 @@
 package Two_D_Array;
 
-public class ColumnWisePrint {
+public class ForEachLoop {
     public static void main(String[] args) {
         int[][] arr = {{6,0,2,7,5},{1,3,7,2,8},{9,3,4,5,2}};
-        for(int j=0;j<arr[0].length;j++){
-            for(int i=0;i<arr.length;i++){
-                System.out.print(arr[i][j]+" ");
+        for(int i=0;i<arr.length;i++){
+            for(int ele:arr[i]){
+                System.out.print(ele+" ");
             }
             System.out.println();
         }
