5d59800ea35b1e8ffd8ae4de82117204efe5d8ae
manish131104 <manishkumar117694@gmail.com>
2025-09-18 12:53:34 +0530

    code

--- src/Two_D_Array/ForEachLoop.java
+++ src/Two_D_Array/ForEachLoop.java
@@ -3,11 +3,17 @@ package Two_D_Array;
 public class ForEachLoop {
     public static void main(String[] args) {
         int[][] arr = {{6,0,2,7,5},{1,3,7,2,8},{9,3,4,5,2}};
-        for(int i=0;i<arr.length;i++){
-            for(int ele:arr[i]){
+        for(int[] a:arr){
+            for(int ele:a){
                 System.out.print(ele+" ");
             }
             System.out.println();
         }
+//        for(int i=0;i<arr.length;i++){
+//            for(int ele:arr[i]){
+//                System.out.print(ele+" ");
+//            }
+//            System.out.println();
+//        }
     }
 }
