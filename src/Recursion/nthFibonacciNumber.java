1d7c3bcdc02274c62d0f84fcbae66069e12ab320
manish131104 <manishkumar117694@gmail.com>
2025-09-27 21:31:53 +0530

    code

---
+++ src/Recursion/nthFibonacciNumber.java
@@ -0,0 +1,16 @@
+package Recursion;
+import java.util.Scanner;
+
+public class nthFibonacciNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n : ");
+        int n= sc.nextInt();
+        int ans=fibo(n);
+        System.out.println(ans);
+    }
+    public static int fibo(int n){
+        if(n<=1) return n;
+        return fibo(n-1)+fibo(n-2);
+    }
+}
