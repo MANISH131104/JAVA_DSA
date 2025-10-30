1e3d1c38273ada8075f0236d6339c4f3008ee43f
manish131104 <manishkumar117694@gmail.com>
2025-10-30 23:34:50 +0530

    code

---
+++ src/LinkedList/NodeOfLinkedList.java
@@ -0,0 +1,34 @@
+package LinkedList;
+
+class Node{
+    int val;        // 0
+    Node next;      // null
+    Node(int val){
+        this.val = val;
+    }
+}
+public class NodeOfLinkedList {
+    public static void main(String[] args) {
+        // 10->20->30->40->50
+        Node a = new Node(10);      // head node
+        Node b = new Node(20);
+        Node c = new Node(30);
+        Node d = new Node(40);
+        Node e = new Node(50);
+
+
+
+        // connect karange (Link Karange)
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+
+        System.out.println(a.next.next.next.val);
+        System.out.println(c);
+        System.out.println(b.next);
+        System.out.println(a.next.next);
+
+
+    }
+}
