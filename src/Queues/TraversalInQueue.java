34493697b08d1269266f852057a8ef941635c0b5
manish131104 <manishkumar117694@gmail.com>
2026-02-11 20:09:40 +0530

    code

---
+++ src/Queues/TraversalInQueue.java
@@ -0,0 +1,79 @@
+package Queues;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TraversalInQueue {
+    private static void display(Queue<Integer> q){
+        int n = q.size();
+        for(int i=1; i<=n; i++){
+            System.out.print(q.peek()+" ");
+            q.add(q.remove());
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        q.add(40);
+        q.add(50);
+        display(q);
+
+        addAtIndex(q,2,60);
+        display(q);
+
+        int peekElement = peek(q,4);
+        System.out.println(peekElement);
+
+        remove(q,2);
+        display(q);
+
+
+    }
+
+    private static void addAtIndex(Queue<Integer> q, int idx, int val) {
+        if(idx<0 || idx>q.size()){
+            System.out.println("Invalid Index! ");
+        }
+        int n = q.size();
+        if(idx<n) {
+            for (int i = 1; i <= idx; i++) {
+                q.add(q.remove());
+            }
+        }
+        q.add(val);
+        for(int i=1; i<=n-idx; i++){
+            q.add(q.remove());
+        }
+    }
+
+    private static int peek(Queue<Integer> q, int idx){
+        if(idx<0 || idx>q.size()){
+            System.out.println("Invalid Index! ");
+        }
+        int n = q.size();
+        for(int i=1; i<=idx; i++){
+            q.add(q.remove());
+        }
+        int peek = q.peek();
+        for(int  i=1; i<=n-idx; i++){
+            q.add(q.remove());
+        }
+        return peek;
+    }
+
+    private static void remove(Queue<Integer> q, int idx){
+        if(idx<0 || idx>q.size()){
+            System.out.println("Invalid Index! ");
+        }
+        int n = q.size();
+        for(int i=1; i<=idx; i++){
+            q.add(q.remove());
+        }
+        int remove = q.remove();
+        for(int i=1; i<=q.size()-idx; i++){
+            q.add(q.remove());
+        }
+    }
+}
