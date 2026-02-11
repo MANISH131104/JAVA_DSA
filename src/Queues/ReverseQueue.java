098af6e37f42a062802e0f2891d3a23dbb4cd282
manish131104 <manishkumar117694@gmail.com>
2026-02-11 20:53:43 +0530

    code

---
+++ src/Queues/ReverseQueue.java
@@ -0,0 +1,30 @@
+package Queues;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class ReverseQueue {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        q.add(40);
+        q.add(50);
+        System.out.println(q);
+
+        Queue<Integer> queue = reverseQueue(q);
+        System.out.println(queue);
+
+    }
+    public static Queue<Integer> reverseQueue(Queue<Integer> q){
+        Stack<Integer> st = new Stack<>();
+        while(q.size()>0){
+            st.push(q.remove());
+        }
+        while(st.size()>0){
+            q.add(st.pop());
+        }
+        return q;
+    }
+}
