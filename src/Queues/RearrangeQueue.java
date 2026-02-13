3099f9c48e9a35643c69aa3162706efcf7c1420b
manish131104 <manishkumar117694@gmail.com>
2026-02-13 10:56:47 +0530

    code

---
+++ src/Queues/RearrangeQueue.java
@@ -0,0 +1,60 @@
+package Queues;
+import java.util.*;
+
+public class RearrangeQueue {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        System.out.print(q+" ");
+        System.out.println();
+
+        rearrangeQueue(q);
+        System.out.print(q+" ");
+    }
+    public static void rearrangeQueue(Queue<Integer> q) {
+        // Implemented by queue
+
+        // Queue<Integer> q2 = new LinkedList<>();
+        // int n = q.size();
+        // for(int i=1; i<=n/2; i++){
+        //     q2.add(q.remove());
+        // }
+        // while(q2.size()>0){
+        //     q.add(q2.remove());
+        //     q.add(q.remove());
+        // }
+
+
+
+        // Implemented by Stacks
+
+        Stack<Integer> st = new Stack<>();
+        int n = q.size();
+        for(int i=1; i<=n/2; i++){
+            st.push(q.remove());
+        }
+        while(st.size()>0){
+            q.add(st.pop());
+        }
+        for(int i=1; i<=n/2; i++){
+            st.push(q.remove());
+        }
+        while(st.size()>0){
+            q.add(st.pop());
+            q.add(q.remove());
+        }
+        while(q.size()>0){
+            st.push(q.remove());
+        }
+        while(st.size()>0){
+            q.add(st.pop());
+        }
+    }
+}
