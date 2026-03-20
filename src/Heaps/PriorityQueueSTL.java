d6c33904bc889fe45598405c08070112b3206b1c
manish131104 <manishkumar117694@gmail.com>
2026-03-20 16:31:05 +0530

    code

--- src/Heaps/PriorityQueueSTL.java
+++ src/Heaps/PriorityQueueSTL.java
@@ -17,8 +17,13 @@ public class PriorityQueueSTL {
 
         System.out.println(pq);
 
+
+        pq.add(5); pq.add(8); pq.add(2); pq.add(0);
+        pq.add(3); pq.add(1); pq.add(7); pq.add(6);
+
         for(int ele: pq){
             System.out.print(ele+" ");
         }
+
     }
 }
