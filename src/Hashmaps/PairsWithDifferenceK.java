706cb81303718101b5cbc004dbe36fe66b91bfd7
manish131104 <manishkumar117694@gmail.com>
2026-04-13 23:04:01 +0530

    code

---
+++ src/Hashmaps/PairsWithDifferenceK.java
@@ -0,0 +1,24 @@
+package Hashmaps;
+import java.util.HashMap;
+
+public class PairsWithDifferenceK {
+    int countPairs(int[] arr, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int ele : arr){
+            if(!map.containsKey(ele)) map.put(ele,1);
+            else{
+                int freq = map.get(ele);
+                map.put(ele,freq+1);
+            }
+        }
+        int pairs = 0;
+        for(int ele : map.keySet()){
+            int rem1 = ele-k;
+            int rem2 = k+ele;
+            if(map.containsKey(rem1)) pairs += (map.get(ele))*(map.get(rem1));
+            if(map.containsKey(rem2)) pairs += (map.get(ele))*(map.get(rem2));
+        }
+        pairs /= 2;
+        return pairs;
+    }
+}
